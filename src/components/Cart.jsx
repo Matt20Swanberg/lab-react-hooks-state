933eef9900bf924ce32da0056d92c7055cbdb6b8
Matthew Swanberg <matt20swanberg@gmail.com>
2026-05-12 14:30:44 -0400

    Render dynamic shopping cart items

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
 
-const Cart = ({cart}) => {
+const Cart = ({ cart }) => {
   return (
     <div>
       <h2>Shopping Cart</h2>
       <ul>
-
+        {cart.map((item) => (
+          <li key={item.id}>{item.name} is in your cart.</li>
+        ))}
       </ul>
     </div>
   )
